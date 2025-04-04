ce939165373eae523bff0a0296b62baf1b2e2daf
sungbin park <terranbin@gmail.com>
2025-04-04 15:43:22 +0900

    Update podman-run-db.sh

--- sqlite3/podman-run-db.sh
+++ sqlite3/podman-run-db.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-podman run -d \
+docker run -d \
   --name service-db \
   -e POSTGRES_USER=sb \
   -e POSTGRES_PASSWORD=sb \
   -e POSTGRES_DB=db \
   -p 5432:5432 \
   -v $PWD/init.sql:/docker-entrypoint-initdb.d/init.sql:ro \
-  docker.io/postgres:14
+  postgres:14
