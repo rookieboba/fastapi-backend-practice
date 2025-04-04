4711de2cebd8488a4449fd9ae4dcdd7f6a4b7b26
sungbin park <terranbin@gmail.com>
2025-04-04 15:43:34 +0900

    Update 01_create_users_table.sql

--- sqlite3/01_create_users_table.sql
+++ sqlite3/01_create_users_table.sql
@@ -1,5 +1,9 @@
 CREATE TABLE users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id SERIAL PRIMARY KEY,
   email TEXT NOT NULL,
   password TEXT NOT NULL
 );
+
+INSERT INTO users (email, password) VALUES
+('test1@example.com', '1234'),
+('test2@example.com', 'abcd');
