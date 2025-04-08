81f2641a738d5b58b01e24e886226f56fe81250b
sungbin <terranbin@gmail.com>
2025-04-08 18:18:52 +0900

    deleted useless lines

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,15 +1,3 @@
 # scripts/init_db.py
-from app.models import User
-from app.database import init_db, get_session
-
+from app.database import init_db
 init_db()
-
-# Optional: insert dummy user
-from sqlmodel import Session
-from app.database import engine
-
-with Session(engine) as session:
-    user = User(name="Sungbin", email="sungbin@example.com")
-    session.add(user)
-    session.commit()
-
